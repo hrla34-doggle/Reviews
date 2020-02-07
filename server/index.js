1b7f142025246fc4ffbbbdbb1f6a981f7eead616
jonathan <mikronium@gmail.com>
2020-02-07 09:40:32 -0800

    changed port to prevent conflicts

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const port = 3002;
+const port = 3001;
 const path = require('path');
 var router = require('./router.js')
 
