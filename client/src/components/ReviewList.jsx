fcc0410c3b93b14be3ea1933bcb53e8a681ce99b
jonathan <mikronium@gmail.com>
2020-02-10 14:35:14 -0800

    fixed styling

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -5,11 +5,22 @@ import Emoji from "./Emoji.jsx"
 const ReviewList = (props) => {
    return(
      <div className ="jp-reviews-2-list">
-      <div className = "jp-reviews-2-name">{props.review.name}</div>
+       <div className = "jp-reviews-2-name-wrapper">
+         <div className = "jp-reviews-2-name-icon"> 
+           <img src ="https://fec-hrla.s3-us-west-1.amazonaws.com/PNG/icon.png" height ="15px" width ="15px" ></img>
+         </div>
+       <div className = "jp-reviews-2-name">{props.review.name}</div>
+       </div>
+      
       <div className = "jp-reviews-2-border"></div>
     
-    <div className = "jp-reviews-2-dates">{props.review.time} </div>
-   
+    <div className = "jp-reviews-2-dates-wrapper">
+      <div className = "jp-reviews-2-dates-svg">
+      <object type = "image/svg+xml" data="https://fec-hrla.s3-us-west-1.amazonaws.com/FEC+SVG/clock2.svg" height = "17px" width = "17px" className = "jp-reviews-2-dates-svg"></object>
+      </div>     
+      <div className = "jp-reviews-2-dates">{props.review.time} 
+      </div>
+    </div>
    <div className = "jp-reviews-2-quotes">
      <div className = "jp-reviews-2-quotes-quote jp-reviews-2-quotes-transform">"</div>
      <div className ="jp-reviews-2-quotes-info">{props.review.quotes}</div>
